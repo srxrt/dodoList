eabb872aa120ac030155c66f72766ea69669d8da
srxrt <str9703@gmail.com>
2024-06-11 14:06:40 +0900

    implemented adding to the tasklist

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,21 +1,57 @@
-class Tasks {
-	tasks = {
-		taskName: "My task",
-		taskId: "1",
-		done: false,
-	};
-}
-
 let taskArray = [];
 
-const taskListElement = document.querySelector(".task-list");
+const taskInput = document.querySelector(".task-input");
+taskInput.focus();
 
-console.log(taskListElement);
+//render list when Enter pressed
+taskInput.addEventListener("keydown", (event) => {
+	console.log(event.key);
+	if (event.key === "Enter") {
+		addTaskToList();
+	}
+});
+
+renderTaskList(taskArray);
+
+const taskListElement = document.querySelector(".task-list");
 
 const checkBox = taskListElement.children[0];
 const labelElement = taskListElement.children[1];
 
 const addTaskButton = document.querySelector(".add-task-button");
+
 addTaskButton.addEventListener("click", () => {
-	labelElement.textContent = "heelow world";
+	addTaskToList();
 });
+
+//input box
+
+//renders the tasklist onto the screen
+function renderTaskList() {
+	const taskListForm = document.querySelector(".task-list");
+	let html = "";
+
+	for (let i = 0; i < taskArray.length; i++) {
+		html += `<div class="task-div">
+            <input type="checkbox" name="a-task" id=${i} />
+			<label for=${i} class="task-name">${taskArray[i].title}</label>
+                  </div>`;
+	}
+
+	taskInput.focus();
+	taskListForm.innerHTML = html;
+}
+
+function addTaskToList() {
+	const title = taskInput.value;
+
+	if (title) {
+		taskArray.push({
+			title,
+			done: false,
+		});
+	}
+
+	taskInput.value = "";
+	renderTaskList(taskArray);
+}
